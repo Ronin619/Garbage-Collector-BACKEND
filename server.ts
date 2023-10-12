72863e08a796f989ea821e1e7e6754ea37d73147
Ronin619 <eytam619@gmail.com>
2023-10-11 22:27:33 -0400

    implement: backbone files

---
+++ server.ts
@@ -0,0 +1,12 @@
+require('dotenv').config({path: "./.env.local"});
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
