1e58743e8e5aeca0a0d652c579134d10a1479570
Ronin619 <eytam619@gmail.com>
2023-10-13 22:07:21 -0400

    implement: mongoose

--- server.ts
+++ server.ts
@@ -1,12 +1,24 @@
-require('dotenv').config({path: "./.env.local"});
+require("dotenv").config({ path: "./.env.local" });
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const cors = require("cors");
 const port = process.env.PORT || 8080;
+
+const mongoURL = process.env.DATABASE_URL;
+mongoose.connect(mongoURL, { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.on("error", (error: string) => {
+  console.log(error);
+});
+db.once("connected", () => {
+  console.log("Database Connected");
+});
 
 app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
+  console.log(`Server running on port: ${port}`);
 });
