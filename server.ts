20dde41aa21897b8b06a84a20afdde4d382f410c
Ronin619 <eytam619@gmail.com>
2023-10-17 21:32:51 -0400

    update: server file with database connection

--- server.ts
+++ server.ts
@@ -4,21 +4,19 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const port = process.env.PORT || 8080;
-
 const mongoURL = process.env.DATABASE_URL;
-mongoose.connect(mongoURL, { useNewUrlParser: true });
-const db = mongoose.connection;
-
-db.on("error", (error: string) => {
-  console.log(error);
-});
-db.once("connected", () => {
-  console.log("Database Connected");
-});
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-});
+// connect to db
+mongoose
+  .connect(mongoURL)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on port: ${port}`);
+    });
+  })
+  .catch((error: string) => {
+    console.log(error);
+  });
